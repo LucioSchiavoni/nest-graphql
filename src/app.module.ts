dd15c525705505e575bacdedc414d8c3b4c59f31
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2023-11-24 11:05:15 -0300

    Fix: deploy database url

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,13 +30,12 @@ dotenv.config()
     }),
     TypeOrmModule.forRoot({
       type:'postgres',
-      host: process.env.DB_HOST,
-      port : parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE, 
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }),TypeOrmModule.forFeature([User, Card, Win]),
    
      UserModule,
